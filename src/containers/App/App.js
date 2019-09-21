c5f8ab3fbae80081e2d405efd2ce4ae1c703924e
Misael Taveras <macrot29@gmail.com>
2019-09-21 15:05:47 -0400

    feat: add theming and prepare test env

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,22 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../component/forms/Button'
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>
+        Clicks: <span data-testid="counter">{count}</span>
+      </h1>
+      <Button onClick={() => setCount(count => count + 1)}>Add</Button>
+    </>
   )
 }
 
